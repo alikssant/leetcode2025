ac085affab119758e307c532c14c477504a7576d
Alex Abatov <a.aliksant315@gmail.com>
2025-03-14 00:48:29 +0900

    leetcode 151

---
+++ leetcode151.js
@@ -0,0 +1,26 @@
+var reverseWords = function (s) {
+  s.trim();
+  let arrayofWords = s.trim().split(" ");
+  let arrayofWordsNospaces = [];
+  arrayofWords.forEach((element) => {
+    if (element.length >= 1 && element[0] !== " ") {
+      arrayofWordsNospaces.push(element);
+    }
+  });
+  let finalresult = "";
+  let i = 0;
+  let j = arrayofWordsNospaces.length - 1;
+  while (i < j) {
+    let temp = arrayofWordsNospaces[i];
+    arrayofWordsNospaces[i] = arrayofWordsNospaces[j];
+    arrayofWordsNospaces[j] = temp;
+    i++;
+    j--;
+  }
+  finalresult = arrayofWordsNospaces.join(" ");
+
+  return finalresult;
+};
+
+s = "       the        sky is   blue       ";
+console.log(reverseWords(s));
