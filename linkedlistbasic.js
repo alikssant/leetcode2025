b4922afd8d81daae20a55dfd86d80298038fa6ce
Alex Abatov <a.aliksant315@gmail.com>
2025-04-23 10:44:17 +0900

    set method added

--- linkedlistbasic.js
+++ linkedlistbasic.js
@@ -114,6 +114,15 @@ class LinkedList {
     }
     return temp;
   }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
 }
 
 let myLinkedList = new LinkedList(1);
